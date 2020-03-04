60d3294d38cd7d0c8f15a65aedaf678d1c5e49fd
IsaiasFernandes <isaiasfernandes1928@hotmail.com>
2020-03-04 20:06:43 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/isaiasfernandes/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.isaiasfernandes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isaiasfernandes.cursomc.domain.Cliente;
+import com.isaiasfernandes.cursomc.repositories.ClienteRepository;
+import com.isaiasfernandes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+	
+}
