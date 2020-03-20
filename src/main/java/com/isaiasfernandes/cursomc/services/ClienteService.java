4829c401e4e5fcb6174d1e76de40c904d36014d3
IsaiasFernandes <isaiasfernandes1928@hotmail.com>
2020-03-20 01:53:29 -0300

    PUT DELETE e GET para Cliente

--- src/main/java/com/isaiasfernandes/cursomc/services/ClienteService.java
+++ src/main/java/com/isaiasfernandes/cursomc/services/ClienteService.java
@@ -1,8 +1,15 @@
 package com.isaiasfernandes.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.isaiasfernandes.cursomc.dto.ClienteDTO;
+import com.isaiasfernandes.cursomc.services.exceptions.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.isaiasfernandes.cursomc.domain.Cliente;
@@ -21,5 +28,37 @@ public class ClienteService {
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		
 	}
-	
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Cliente porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObjt, Cliente obj) {
+		newObjt.setNome(obj.getNome());
+		newObjt.setEmail(obj.getEmail());
+	}
 }
