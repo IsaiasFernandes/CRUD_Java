bd10b768b158ec66da243ee6b505a90301016cc8
IsaiasFernandes <isaiasfernandes1928@hotmail.com>
2020-03-23 22:00:19 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/isaiasfernandes/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/isaiasfernandes/cursomc/dto/ClienteDTO.java
@@ -16,8 +16,8 @@ public class ClienteDTO implements Serializable {
     @Length(min=5, max=120, message="O tamanho deve ser entre 5 a 120 caracteres")
     private String nome;
 
-    @NotEmpty(message="Preenchimento obrigatório")
-    @Email(message="Email inválido")
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
     private String email;
 
     public ClienteDTO() {
---
+++ src/main/java/com/isaiasfernandes/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.isaiasfernandes.cursomc.services.validation;
+
+import com.isaiasfernandes.cursomc.domain.enums.TipoCliente;
+import com.isaiasfernandes.cursomc.dto.ClienteNewDTO;
+import com.isaiasfernandes.cursomc.resources.exceptions.FieldMessage;
+import com.isaiasfernandes.cursomc.services.validation.utils.BR;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+        if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
