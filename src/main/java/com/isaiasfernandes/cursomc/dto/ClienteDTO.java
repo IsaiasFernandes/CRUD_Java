fb4761ee7c2c234e1ec3404a2c7a28b9f8d0ba19
IsaiasFernandes <isaiasfernandes1928@hotmail.com>
2020-03-23 23:05:43 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/isaiasfernandes/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/isaiasfernandes/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.isaiasfernandes.cursomc.dto;
 
 import com.isaiasfernandes.cursomc.domain.Cliente;
+import com.isaiasfernandes.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
